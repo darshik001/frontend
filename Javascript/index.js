01a1d450aebc6e5972655c44cdc8e2eba08dc175
darshik007 <darshikshekhada07@gmail.com>
2025-05-14 12:56:57 +0530

    14/05/25

---
+++ Javascript/index.js
@@ -0,0 +1,40 @@
+// console.log("hello");
+// console.log(10+20-30);
+// console.warn("this is warning msg")
+// console.error("this is error msg")
+
+// document.writeln("hello world")
+
+// alert("this is aelert msg")
+
+// document.getElementById("heding").innerHTML="this is h1 tag"
+// document.getElementById("heding").textContent="this is h1 tag1"
+
+
+// VERIABLE
+
+// var let const 
+
+// var a = 10;
+// console.log(a);
+// var a = 20;
+// console.log(a);
+
+
+// let a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// const a = 10
+// console.log(a);
+// a = 20;
+
+// DATA TYPE 
+
+// -->  PRIMETIVE DATA-TYPE
+    // string , number , BigInt ,Boolean, undefined , null , Symbol
+
+
+// --> NON-PRIMETIVE DATA-TYPE 
+//  object
---
+++ Javascript/opretor.js
@@ -0,0 +1,40 @@
+
+
+// OPERATORS 
+
+// --> ARITHMETIC OPERATORS
+
+// + , - , * , / , %(modulus(remanider)) , **(Power) , ++ , --
+// console.log(10+5);
+// console.log(10-5);
+// console.log(10*5);
+// console.log(10/7);
+// console.log(10%3);
+
+// console.log(2**3);
+
+// let a =12
+// a++;
+// console.log(a);
+// console.log(++a);
+
+
+// --> COMPARISON OPERATORS 
+
+// < <= > >= == != === !==
+
+
+// console.log(10<20);
+// console.log(10<=20);
+
+// console.log(10>5);
+// console.log(10>=5);
+
+// console.log(10==10);
+// console.log(10=="10");
+// console.log(10!=10);
+
+// console.log(10===10);
+// console.log(10==="10");
+// console.log(10!=="10");
+
