15444c9f7e4a1450f108cc33c88c457cb8ea9b34
darshik007 <darshikshekhada07@gmail.com>
2025-05-16 14:37:50 +0530

    16/05

--- Javascript/opretor.js
+++ Javascript/opretor.js
@@ -38,3 +38,129 @@
 // console.log(10==="10");
 // console.log(10!=="10");
 
+
+
+//1. WAP TO SWAPE A TWO NUMBER
+
+// let a = 10 ,b = 20 , c;
+
+// c = a ;
+// a = b;
+// b=c;
+
+// a = a+b;
+// b = a-b;
+// a = a- b;
+
+// a = a*b;
+// b = a/b;
+// a = a/b;
+
+// a = a^b;
+// b = a^b;
+// a = a^b;
+
+// console.log("afetr swaping A and B");
+// console.log("A==>",a);
+// console.log("B==>",b);
+
+
+
+// 2.WAP TO FINDE OUT GROS SALLRY 
+
+// let basicsallry= 10000,gs,hra,da;
+ 
+//      hra = basicsallry*0.10;
+//      da = basicsallry*0.05;
+//      gs = basicsallry+hra+da;
+
+//      console.log("basic sallray:",basicsallry);
+//      console.log("HRA:",hra);
+//      console.log("DA:",da);
+//      console.log("Gross Sallry:",gs);
+
+
+
+//  3.WAP TO CALCULATE INTREST
+
+    // let la= 100000,roi=1.5,time=12,intrest;
+    //   intrest = la*roi*time /100;
+    //    la = la+ intrest;
+    //   console.log("intest pay:",intrest);
+    //   console.log("Total paybal amount :",la);
+      
+
+
+  // 4.WAP TO PRINT A BILL
+
+    //  let totalamount,sgst,cgst,discount,amount=1000;
+    //  discount = amount *2/100;
+    //  totalamount = amount - discount;
+    //  sgst = totalamount *9 /100;
+    //  cgst = totalamount *9 /100;
+    //  totalamount =totalamount+ sgst +cgst;
+
+    //  console.log("Your Bill    :",amount);
+    //  console.log("Discount (2%):",discount);
+    //  console.log("SGST (9%)    :",sgst);
+    //  console.log("CGST (9%)    :",cgst);
+    //  console.log("Total bill   :",totalamount);
+
+
+
+    // 5. WAP TO FIND OUT AREA OF TRIANGLE;
+
+    // let triangle,basi=10,height = 12;
+    // triangle = (1/2) *basi * height
+    // console.log("Area of Trinagle:",triangle);
+    
+
+
+    // 6. WAP TO FIND OUT AREA OF CIRCULE;
+
+    // let radius =5 ,circule;
+    // circule = 3.14*(radius*radius);
+    // console.log("Area of Circule:",circule);
+    
+     
+      
+    // 7. WAP TO FIND OUT AREA OF RECTANGLE;
+
+    // let rectangle,width=10;height=20;
+    // rectangle = width*height;
+    // console.log("rectangle is:",rectangle);
+
+
+
+    // 8 WAP TO CONVERT DOLLER INTO RUPEES 
+
+    // let doller = 268 , rupeesprice=85.61, rupees;
+    // rupees = doller * rupeesprice;
+
+    // console.log("Total Rupees:",rupees);
+  
+    
+
+    // 9. WAP TO CONVERT FEET INTO INCHES
+
+    // let feet = 15 ,inches;
+    // inches = feet*12;
+
+    // console.log("Inches:",inches)
+     
+
+    // 10. WAP TO CONVERT FEET INTO METERS
+
+
+  //  let feet = 12 ,meters;
+  //  meters =feet * 0.3048;
+
+  //   console.log("Inches:",meters)
+
+  // 11. WAP TO CONVERT CELCUIS INTO FEHERNHIT 
+
+    //  let celcuis = 20, fehernhit;
+    //  fehernhit = (celcuis * 9/5) +32;
+
+    // console.log("Fehernhit:",fehernhit)
+ 
