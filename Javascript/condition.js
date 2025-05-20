d78a0073a692b72288f9fd39c833fe3d14d346c9
darshik007 <darshikshekhada07@gmail.com>
2025-05-20 15:33:40 +0530

    20/05/25

---
+++ Javascript/condition.js
@@ -0,0 +1,186 @@
+
+
+// if, if else , nested , ladder , switch , ternary 
+
+
+// 1.if  and if else and ternary
+/*
+syntex
+      if(condition){
+      //  statement 
+      }
+*/
+
+//  let a =12;
+// if(a%2==0){
+//   console.log("even Number");
+// }
+
+/*
+syntex
+      if(condition){
+      //  statement 
+      } else{
+      //  statement 
+       }
+*/
+
+    // let a =12;
+// if(a%2==0){
+//   console.log("even Number");
+// }  else{
+//   console.log("odd Number");
+// }
+
+
+
+/* 
+ syntex:
+
+    condition ? true:false
+*/
+    // let a =12;
+  // a%2==0 ?console.log("even Number"):console.log("odd Number");
+  
+     
+
+
+
+// 2.nested if else
+
+/*
+   syntex
+
+   if(condition){
+
+      if(condition){
+        statment
+      } else{
+       }
+
+   }   else{
+
+       if(condition){
+        statment
+      } else{
+       }
+
+      }
+ */
+
+
+    // let a=10,b=23,c=45;
+
+    // if(a>b){
+    //      if(a>c){
+    //       console.log("a is max");
+    //      }  else{
+    //        console.log("c is max");
+    //      }
+    // }  else{
+    //     if(b>c){
+    //       console.log("b is max");
+    //     }  else{
+    //       console.log("c is maximum");
+          
+    //     }
+    // }
+
+
+
+
+
+
+
+
+                                                                   //PROGRAM
+
+    // 1.Enter two value find smallest.
+      
+        // let a =20, b =85;
+        // if(a<b){
+        //   console.log("a is small");
+        // } else{
+        //   console.log("b is small");
+        // }
+
+
+// 2. Enter one value find its positive or negative.
+
+    //  let a;
+    //  a = Number(prompt("Enter value"));
+
+    // if(a>=0){
+    //    console.log("Positive Number");
+    // }  else{
+    //    console.log("Negative Number");
+    // }
+
+
+// 3. Enter one value find its divisible by 5 or not.
+  
+    //  let a;
+    //  a = Number(prompt("Enter value"));
+
+    //  if(a%5==0){
+    //     console.log(a,"this number is divisible by 5");
+    //  } else{
+    //     console.log(a,"this number is not divisible by 5");
+    //  }
+
+
+// 4.Enter one value find its divisible by 5 and 3 or not.
+   
+
+    //   let a;
+    //  a = Number(prompt("Enter value"));
+
+    //  if(a%5==0 || a%3==0){
+    //     console.log(a,"this number is divisible by 5 and 3");
+    //  } else{
+    //     console.log(a,"this number is not divisible by 5 and 3");
+    //  }
+
+
+// 5.Enter Students marks wheather pass or not
+
+  //  let marks;
+  //  marks = parseInt(prompt("Enter marks to check pass or not",marks));
+
+  //  if(marks>=33){
+  //      console.log("This student is pass");
+  //  } else{
+  //      console.log("This student is faile");     
+  //  }
+
+
+
+// 6.Enter year find its leap year or not
+
+//  let year = 1996;
+
+// if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+//   console.log(year + " is a leap year.");
+// } else {
+//   console.log(year + " is not a leap year.");
+// }
+
+
+// 7. Enter a, b, c find minimum among them
+
+    // let a = 10,b=20,c=30;
+
+    //   if(a>b){
+    //      if(a>c){
+    //       console.log("a is max");
+    //      }  else{
+    //        console.log("c is max");
+    //      }
+    // }  else{
+    //     if(b>c){
+    //       console.log("b is max");
+    //     }  else{
+    //       console.log("c is maximum");
+          
+    //     }
+    // }
