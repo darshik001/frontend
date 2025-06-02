394af0c5adc4a1fc36020fa5e9617ea2738f58be
darshik007 <darshikshekhada07@gmail.com>
2025-06-02 16:47:35 +0530

    02/06/25

---
+++ Javascript/array.js
@@ -0,0 +1,58 @@
+// Array is collection of Element
+
+// Decleration
+
+// let a = new Array(10,20,30,40,50)
+//   let a = [10,20,30,40,50]
+// console.log(a)
+
+// a[1] = 60
+// a[10] = 500
+// a[7] = 300
+// console.log(a)
+
+// console.log(a.length)
+
+// for(let i =0;i<a.length;i++){
+//   console.log(a[i])
+// }
+
+
+
+
+                                      //*  ARRAY'S  METHODS  *\\
+
+
+
+// let data = [10,20,30,40,50]                                            
+
+// // push method => add data in last position 
+// console.log(data) 
+
+// data.push(60)
+// data.push(70,80,90)
+// console.log(data)
+
+
+// // pop method => delete(remove) data in last position 
+
+// data.pop()      // onliy one data delete
+// data.pop()
+// data.pop()
+// console.log(data) 
+
+
+// // unshift method => add data in first position 
+
+// data.unshift(5)
+// data.unshift(2,3,4)
+// console.log(data)
+
+
+// // shift method => add data in first position 
+
+
+// data.shift()   // onliy one data delete
+// data.shift()
+// console.log(data)
+
