9c3e4800fa0ce21a04c9e4171a97504de16252a7
darshik007 <darshikshekhada07@gmail.com>
2025-06-10 17:51:29 +0530

    10/06/25

--- Javascript/object.js
+++ Javascript/object.js
@@ -37,6 +37,66 @@
 
 
 
+// let person = {
+//   name:"darshik",
+//   age:23,
+//   hobbies:["reding","traveling","music"],
+//   address: {
+//     shop_no:436,
+//     bulding_name:"sayona plza",
+//     lendmark:"near silver showk",
+//     city:"surat"
+//   }
+// }
+
+// console.log(person)
+
+// for(let key in person){
+//   if(key=="address"){
+//     let add = ""
+//     for(let key in person.address){
+//        add += person.address[key] + ","
+//     }
+//     console.log(`${key} => ${add}`)
+//   }else{
+//     console.log(`${key}=>${person[key]}`)
+//   }
+// }
 
 
+// console.log(person.hobbies[0]);
+// console.log(person.address.city)
+
+
+
+
+// for in (object,array) and for of (array)
+
+
+
+// 1. for in loop 
+
+// 1. object for in loop
+// for(let key in person){
+//   console.log(`${key} =>${person[key]}`)
+// }
   
+
+// 2. array for in loop
+
+  // let arr = [11,22,33,44,55]
+
+  // for(let key in arr){
+  //   console.log(arr[key])
+  // }
+
+
+
+  // 2 for of loop 
+
+      //  for of loop  use onliy array
+
+  //          for(let val of arr){
+  //   console.log(val)
+  // }
+
