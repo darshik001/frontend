a04f4c13a3e499f34ab79bb56c0bc7aa173eeecd
darshik007 <darshikshekhada07@gmail.com>
2025-06-30 20:01:34 +0530

    30/06/25

---
+++ Javascript/timing-function/scrpt.js
@@ -0,0 +1,27 @@
+
+
+
+
+/*  one time Exicute */
+// console.log("hello word 1")
+// console.log("hello word 2")
+// let id =  setTimeout(()=>{
+
+//   console.log("hello word 3")
+// },3000)
+// console.log("hello word 4")
+
+
+/* Break Exicute*/
+// clearTimeout(id)
+
+// console.log("hello word 1")
+// console.log("hello word 2")
+// let id = setInterval(()=>{
+
+//   console.log("hello word 3")
+// },3000)
+// console.log("hello word 4")
+
+/* Break Exicute*/
+// clearInterval(id)
