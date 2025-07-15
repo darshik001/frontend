ad9f6d367318b00352f0177c1c6fc2d75460baf7
darshik007 <darshikshekhada07@gmail.com>
2025-07-15 13:46:49 +0530

    15/07/25

--- PROJECT/JS-PROJECT/weather api/js/script.js
+++ PROJECT/JS-PROJECT/weather api/js/script.js
@@ -1,21 +1,47 @@
+let searchdata = document.querySelector("#search-input")
 
 
-document.querySelector('#search').addEventListener("click",()=>{
-   let searchdata = document.querySelector("#search-input").value
-   let weather = document.querySelector('#weather')
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchdata}&appid=728553c5759e12c0b5c96e66500b755b&units=metric`)
-  .then(res => res.json())
-  .then(data => {
-    
-    let row = `
-    <div class="current-weather">
+let timefunction = () => {
+    let date = new Date()
+    return (date.toDateString())
+}
+
+function formatTime(timestamp) {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+
+let weatherfunction = (searchdata) => {
+
+    let weather = document.querySelector('#weather')
+    let data;
+    if (searchdata.value == "") {
+        data = "surat"
+    } else {
+        data = searchdata.value
+    }
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=728553c5759e12c0b5c96e66500b755b&units=metric`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.cod == 404) {
+                weather.innerHTML = `<h2 class="text-center mt-5 pt-5"> Data Not Found</h2>`
+            } else {
+
+
+                let sunsetTime = formatTime(data.sys.sunset);
+                let sunriseTime = formatTime(data.sys.sunrise);
+
+
+                let row = `
+    <div class="current-weather my-auto">
             <div class="location">${data.name}, ${data.sys.country}</div>
-            <div class="date">${new Date()}</div>
+            <div class="date">${timefunction()}</div >
             
             <div class="weather-main">
                 <div>
-                    <div class="temperature">${data.main.temp}°C</div>
-                    <div class="condition">${data.weather[0].main}</div>
+                    <div class="temperature">${Math.floor(data.main.temp)}°C</div>
+                    <div class="condition">${data.weather[0].description}</div>
                 </div>
                 <img src="https://cdn.weatherbit.io/static/img/icons/c02d.png" alt="Weather Icon" class="weather-icon">
             </div>            
@@ -34,13 +60,14 @@ document.querySelector('#search').addEventListener("click",()=>{
                         <div>${data.wind.speed} km/h</div>
                     </div>
                 </div>
-                <div class="detail-item">
-                    <div class="detail-icon">🔆</div>
-                    <div>
-                        <div>UV Index</div>
-                        <div>5 of 10</div>
+                 <div class="detail-item">
+                    <div class="detail-icon">☁️</div>
+                    <div class="detail-text">
+                        <div class="detail-label">Clouds</div>
+                        <div class="detail-value">${data.clouds.all}%</div>
                     </div>
                 </div>
+                
                 <div class="detail-item">
                     <div class="detail-icon">👁️</div>
                     <div>
@@ -48,10 +75,40 @@ document.querySelector('#search').addEventListener("click",()=>{
                         <div>${data.visibility / 1000} km</div>
                     </div>
                 </div>
+                <div class="detail-item">
+                    <div class="detail-icon">🔆</div>
+                    <div>
+                        <div>sunrise</div>
+                        <div>${sunriseTime}</div>
+                    </div>
+                </div>
+
+                <div class="detail-item">
+                    <div class="detail-icon">🌙</div>
+                    <div>
+                        <div>Sunset</div>
+                        <div>${sunsetTime}</div>
+                    </div>
+                </div>
+
+    
             </div>
-        </div>
-    `
-    weather.innerHTML = row
-  })
-  .then(err=> console.log(err.message))
+        </div >
+            `
+                weather.innerHTML = row
+
+
+            }
+            searchdata.value = ""
+        })
+        .catch(err => console.log(err))
+
+
+}
+
+weatherfunction(searchdata)
+
+document.querySelector('#search').addEventListener("click", () => {
+
+    weatherfunction(searchdata)
 })
